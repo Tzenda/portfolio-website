e2bab2e4e14073469c79e8dff4766bcff89769cc
Tzenda <robertchen9@gmail.com>
2022-07-08 00:25:02 +0200

    weather app

--- js/weather-app.js
+++ js/weather-app.js
@@ -1,12 +1,6 @@
 let weather = {
-  apiKey: "207821d74f9580114631db0bdcdaa0e7",
   fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey
-    )
+    fetch("/js/weather-app-server/weather-app-serve.js")
       .then((response) => response.json())
       .then((data) => this.displayWeather(data));
   },
